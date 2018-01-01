0d7ae9e5171d536803335543dc78ee7a374966e0
ajoshow <ajoshow@gmail.com>
2018-01-01 23:21:04 +0800

    add more excrises

--- src/main/java/com/ajoshow/exam1z0813/DaylightSavingMain.java
+++ src/main/java/com/ajoshow/exam1z0813/DaylightSavingMain.java
@@ -11,7 +11,7 @@ public class DaylightSavingMain {
     public static void main(String[] args) {
 
         // ld1 and ld2 has -1 hour in difference (ld2 is before ld1),
-        // since zd1 has 1 hour daylight saving in US/Eastern, which means it has additional hour (2AM->1AM, 3AM->2AM)
+        // since zd1 has 1 hour daylight saving in US/Eastern, one hour less (2AM->1AM, 3AM->2AM)
         // so think if ld1 is 2015-11-1T02:00, zd1 must be 2015-11-1T03:00 in order to be the same as ld1. (3AM -> 2AM)
         // thus, zd1 and zd2 should have -2 hour in difference.
 
@@ -20,11 +20,19 @@ public class DaylightSavingMain {
         System.out.println("ld1: " + ld1);
         ZonedDateTime zd1 = ZonedDateTime.of(ld1, ZoneId.of("US/Eastern"));
         System.out.println("zd1: " + zd1);
+
         LocalDateTime ld2 = LocalDateTime.of(2015, Month.NOVEMBER, 1, 1, 0);
         System.out.println("ld2: " + ld2);
         ZonedDateTime zd2 = ZonedDateTime.of(ld2, ZoneId.of("US/Eastern"));
         System.out.println("zd2: " + zd2);
+
         long x = ChronoUnit.HOURS.between(zd1, zd2);
         System.out.println(x); // -2
+
+
+        ZonedDateTime zd3 = ZonedDateTime.of(ld1, ZoneId.systemDefault());
+        System.out.println("zd3: " + zd3);
+        long y = ChronoUnit.HOURS.between(zd3, zd2);
+        System.out.println(y); // -2
     }
 }
